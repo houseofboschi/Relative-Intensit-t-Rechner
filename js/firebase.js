4c1884cc0756d0b189e25556d9b1c6e3a299e99d
houseofboschi <houseofboschi@gmail.com>
2026-02-28 17:31:13 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyDMAmfE-kfnX-g1T08Ad0Uw3kmLKs2fH30",
+  authDomain: "ri-calculator.firebaseapp.com",
+  projectId: "ri-calculator",
+  storageBucket: "ri-calculator.firebasestorage.app",
+  messagingSenderId: "640294756569",
+  appId: "1:640294756569:web:e9902b040c6749f7abdec8"
 };
 
 const app = initializeApp(firebaseConfig);
